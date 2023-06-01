978efcc26d34edb59b29e02414ec05f34b3edf50
HassanAbdelbaset <hassanelfalt60@gmail.com>
2023-06-01 23:30:20 +0300

    details ui

--- lib/Screen/Details.dart
+++ lib/Screen/Details.dart
@@ -9,8 +9,8 @@ class Details extends StatefulWidget {
 }
 
 class _DetailsState extends State<Details> {
-  var imgPath = "https://image.tmdb.org/t/p/w300/";
-
+  var imgPath = "https://image.tmdb.org/t/p/w500/";
+/*
   @override
   void initState() {
     print("details init");
@@ -27,25 +27,139 @@ class _DetailsState extends State<Details> {
   void dispose() {
     print("details dispose");
     super.dispose();
-  }
+  }*/
 
   @override
   Widget build(BuildContext context) {
     var singleMovie = ModalRoute.of(context)?.settings.arguments as Movie;
     return Scaffold(
-      appBar: AppBar(),
-      body: SingleChildScrollView(
-        child: Column(crossAxisAlignment: CrossAxisAlignment.center, children: [
-          Image.network(imgPath + singleMovie.backdropPath.toString()),
-          Text(
-            singleMovie.originalTitle.toString(),
-            style: const TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+      backgroundColor: Colors.black,
+      body:
+          Column(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
+        Container(
+          height: 350,
+          width: double.infinity,
+          decoration: BoxDecoration(
+            image: DecorationImage(
+                image:
+                    NetworkImage(imgPath + singleMovie.backdropPath.toString()),
+                fit: BoxFit.fill),
           ),
-          Text(singleMovie.overview.toString()),
-          Text(singleMovie.releaseDate.toString()),
-          Text("Popularity: ${singleMovie.popularity}"),
-        ]),
-      ),
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Container(
+              height: 35,
+              width: 40,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: Colors.blueGrey),
+              child: const Center(
+                child: Text(
+                  "+18",
+                  style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white),
+                ),
+              ),
+            ),
+            Container(
+              height: 35,
+              width: 60,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: Colors.blueGrey),
+              child: const Center(
+                child: Text(
+                  "Action",
+                  style: TextStyle(
+                      fontSize: 16,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white),
+                ),
+              ),
+            ),
+            Container(
+              height: 35,
+              width: 60,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: Colors.blueGrey),
+              child: Center(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      singleMovie.voteAverage.toString(),
+                      style: const TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.bold,
+                          color: Colors.white),
+                    ),
+                    const Icon(
+                      Icons.star,
+                      color: Colors.yellow,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            const SizedBox(
+              width: 35,
+            ),
+            Container(
+              height: 35,
+              width: 35,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: Colors.blueGrey),
+              child: const Center(
+                child: Icon(Icons.add),
+              ),
+            ),
+            Container(
+              height: 35,
+              width: 35,
+              decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(10),
+                  color: Colors.blueGrey),
+              child: const Center(
+                child: Icon(Icons.share),
+              ),
+            ),
+          ],
+        ),
+        Container(
+          margin: const EdgeInsets.fromLTRB(15, 0, 15, 0),
+          alignment: Alignment.centerLeft,
+          child: Text(
+            singleMovie.originalTitle.toString(),
+            style: const TextStyle(
+                fontSize: 18, fontWeight: FontWeight.bold, color: Colors.white),
+          ),
+        ),
+        Container(
+          margin: const EdgeInsets.fromLTRB(15, 0, 15, 0),
+          child: Text(
+            singleMovie.overview.toString(),
+            style: const TextStyle(color: Colors.grey),
+          ),
+        ),
+        Text(
+          singleMovie.releaseDate.toString(),
+          style: const TextStyle(color: Colors.white),
+        ),
+        Text(
+          "Popularity: ${singleMovie.popularity}",
+          style: const TextStyle(color: Colors.white),
+        ),
+        SizedBox(
+          height: 45,
+        )
+      ]),
+      //  ),
     );
   }
 }
--- lib/Screen/Home.dart
+++ lib/Screen/Home.dart
@@ -10,7 +10,7 @@ class Home extends StatefulWidget {
 
 class _HomeState extends State<Home> {
   var imgPath = "https://image.tmdb.org/t/p/w300/";
-
+/*
   @override
   void deactivate() {
     print("home deactivate");
@@ -21,104 +21,109 @@ class _HomeState extends State<Home> {
   void dispose() {
     print("home dispose");
     super.dispose();
-  }
+  }*/
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
-      body: Padding(
+      body: SingleChildScrollView(
         padding: const EdgeInsets.fromLTRB(30, 50, 30, 0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text("Coming Soon",
-                style: TextStyle(
-                    fontWeight: FontWeight.bold,
-                    fontSize: 25,
-                    color: Colors.white)),
-            Padding(
-              padding: const EdgeInsets.fromLTRB(0, 30, 0, 0),
-              child: Image.network(
-                  "https://people.com/thmb/WyIht3TarwbXSUpZVGmkcEmh2kk=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc():focal(999x0:1001x2)/lion-king-11-2000-0f18b8c334de400c9520a09f94c4a335.jpg",
-                  width: double.infinity),
-            ),
-            Padding(
-                padding: const EdgeInsets.fromLTRB(0, 30, 0, 30),
-                child: Text("Trending Now",
-                    style: TextStyle(
-                        fontWeight: FontWeight.bold,
-                        fontSize: 25,
-                        color: Colors.white))),
-            Expanded(
-              child: ListView.builder(
-                
-                itemCount: APIHelper.moviesInApp.length,
-                scrollDirection: Axis.horizontal,
-                itemBuilder: (context, index) => Padding(
-                  padding: const EdgeInsets.fromLTRB(15.0,0,15,0),
-                  child: Column(children: [
-                    InkWell(
-                      onTap: () {
-                        Navigator.pushNamed(context, "details",
-                            arguments: APIHelper.moviesInApp[index]);
-                      },
-                      child: Image.network(
-                          imgPath +
-                              APIHelper.moviesInApp[index].posterPath.toString(),
-                          height: 350),
-                    ),
-                    Padding(
-                      padding: const EdgeInsets.fromLTRB(0,10,0,0),
-                      child: Row(
-                        
-                        children: [
-                          Padding(
-                            padding: const EdgeInsets.fromLTRB(0,0,20,0),
-                            child: Text("Action",
-                                style: TextStyle(
-                                    fontWeight: FontWeight.bold,
-                                    fontSize: 16,
-                                    color: Colors.white,
-                                    backgroundColor: Colors.grey[700]
-                                    // decoration:TextDecoration(TextDecoration: OutlineInputBorder(borderRadius: BorderRadius.circular(10))) 
-                                  )
-                              ),
-                          ),
-                            
-                          Container(
-                            decoration: BoxDecoration(color: Colors.grey[700]),
-                            child: Row(
-                              children: [
-                                
-                                Icon(Icons.star,color: Colors.yellow,),
-                                Text( APIHelper.moviesInApp[index].voteAverage.toString(),
-                                    style: TextStyle(
+        child: SizedBox(
+          height: 750,
+          child: Column(
+            mainAxisSize: MainAxisSize.min,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              const Text("Coming Soon",
+                  style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 25,
+                      color: Colors.white)),
+              Padding(
+                padding: const EdgeInsets.fromLTRB(0, 30, 0, 0),
+                child: Image.network(
+                    "https://people.com/thmb/WyIht3TarwbXSUpZVGmkcEmh2kk=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc():focal(999x0:1001x2)/lion-king-11-2000-0f18b8c334de400c9520a09f94c4a335.jpg",
+                    width: double.infinity),
+              ),
+              const Padding(
+                  padding: EdgeInsets.fromLTRB(0, 30, 0, 30),
+                  child: Text("Trending Now",
+                      style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 25,
+                          color: Colors.white))),
+              Expanded(
+                child: ListView.builder(
+                  itemCount: APIHelper.moviesInApp.length,
+                  scrollDirection: Axis.horizontal,
+                  itemBuilder: (context, index) => Padding(
+                    padding: const EdgeInsets.fromLTRB(15.0, 0, 15, 0),
+                    child: Column(children: [
+                      InkWell(
+                        onTap: () {
+                          Navigator.pushNamed(context, "details",
+                              arguments: APIHelper.moviesInApp[index]);
+                        },
+                        child: Image.network(
+                            imgPath +
+                                APIHelper.moviesInApp[index].posterPath
+                                    .toString(),
+                            height: 350),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.fromLTRB(0, 10, 0, 0),
+                        child: Row(
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.fromLTRB(0, 0, 20, 0),
+                              child: Text("Action",
+                                  style: TextStyle(
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 16,
+                                      color: Colors.white,
+                                      backgroundColor: Colors.grey[700]
+                                      // decoration:TextDecoration(TextDecoration: OutlineInputBorder(borderRadius: BorderRadius.circular(10)))
+                                      )),
+                            ),
+                            Container(
+                              decoration:
+                                  BoxDecoration(color: Colors.grey[700]),
+                              child: Row(
+                                children: [
+                                  const Icon(
+                                    Icons.star,
+                                    color: Colors.yellow,
+                                  ),
+                                  Text(
+                                      APIHelper.moviesInApp[index].voteAverage
+                                          .toString(),
+                                      style: const TextStyle(
                                         fontWeight: FontWeight.bold,
                                         fontSize: 15,
                                         color: Colors.white,
-                                        
-                                        // decoration:TextDecoration(TextDecoration: OutlineInputBorder(borderRadius: BorderRadius.circular(10))) 
-                                      )
-                                  ),
-                              ],
+
+                                        // decoration:TextDecoration(TextDecoration: OutlineInputBorder(borderRadius: BorderRadius.circular(10)))
+                                      )),
+                                ],
+                              ),
                             ),
-                          ),
-                        ],
+                          ],
+                        ),
                       ),
-                    ),
-                    Text(
-                      APIHelper.moviesInApp[index].originalTitle.toString(),
-                      style: const TextStyle(
-                          fontSize: 18,
-                          fontWeight: FontWeight.bold,
-                          color: Colors.white),
-                    ),
-                  ]),
+                      Text(
+                        APIHelper.moviesInApp[index].originalTitle.toString(),
+                        style: const TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                            color: Colors.white),
+                      ),
+                    ]),
+                  ),
                 ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
